b5a8e5650d8ac56daba968668a45b691cd0900b1
unknown <zoleikha.works@gmail.com>
2023-08-20 19:46:46 +0200

    PayrollGenerator with Service

---
+++ src/Command/GeneratePayrollCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Service\PayrollGenerator;
+
+class GeneratePayrollCommand extends Command
+{
+    private PayrollGenerator $payrollGenerator;
+
+    public function __construct(PayrollGenerator $payrollGenerator)
+    {
+        $this->payrollGenerator = $payrollGenerator;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('payroll:generate')
+            ->setDescription('Generate payroll CSV')
+            ->addArgument('filename', InputArgument::REQUIRED, 'Output CSV filename');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filename = $input->getArgument('filename');
+        
+        $this->payrollGenerator->generateCSV($filename);
+        
+        $output->writeln('Payroll CSV generated successfully.');
+        
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Service/PayrollGenerator.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Service;
+
+class PayrollGenerator
+{
+
+    public function getNextSalaryPaymentDate($date) {
+        // F will output the month in a word, For example, January.
+        // Y will output the year using 4 digits. For example, 1993
+        $lastDayOfMonth = strtotime('last day of month' . date('F Y', $date));
+        // check if the day of the week of the last day of the month is Saturday (6) or Sunday (7) (if it's a weekend).
+        if (date('N', $lastDayOfMonth) >= 6) {
+            return strtotime('last Friday of ' . date('F Y', $date));
+        }
+        return $lastDayOfMonth;
+    }
+
+    public function getNextBonusPaymentDate($date) {
+        $fifteenthOfMonth = strtotime('15th' . date('F Y', $date));
+        if (date('N' , $fifteenthOfMonth) >= 6) {
+            return strtotime('next Wednesday', $date);
+        }
+        return $fifteenthOfMonth;
+    }
+    
+    public function generateCSV(string $filename)
+    {       
+        // Array of month, salary date, and bonus date
+        $csvData = []; 
+        
+        $currentDate = time();
+
+        for ($i = 0; $i < 12; $i++) {
+            $monthName = date('F', $currentDate);
+
+            $salalyDate = $this->getNextSalaryPaymentDate($currentDate);
+            $bonusDate = $this->getNextBonusPaymentDate($currentDate);
+
+            $csvData[] = [$monthName, date('Y-m-d', $salalyDate), date('Y-m-d', $bonusDate)];
+
+            $currentDate = strtotime('+1 month', $currentDate);
+        }
+        
+        $handle = fopen($filename, 'w');
+        fputcsv($handle, ['Month', 'Salary Date', 'Bonus Date']);
+        
+        foreach ($csvData as $row) {
+            fputcsv($handle, $row);
+        }
+        
+        fclose($handle);
+    }
+}
